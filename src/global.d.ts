3c4867bb9037e5fe02bccfbe6eec26c91c260b9b
Jordan Rancie <8184815+jordanrancie@users.noreply.github.com>
2025-07-31 20:56:58 +0800

    env variables

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,4 +8,22 @@ declare global {
 	}
 }
 
-export {};
+/// <reference types="@sveltejs/kit" />
+declare module '$env/static/public' {
+    export const PUBLIC_UI_MOUNT_POINT: string;
+    export const PUBLIC_WEB_MOUNT_POINT: string;
+    export const PUBLIC_DOMAIN: string;
+    export const PUBLIC_DOMAIN_BASE_URL: string;
+	export const PUBLIC_AUTH0_CALLBACK_URL: string;
+	export const PUBLIC_AUTH0_LOGOUT_URL: string;
+	export const PUBLIC_AUTH0_CLIENT_ID: string;
+	export const PUBLIC_AUTH0_DOMAIN: string;
+}
+
+/// <reference types="@sveltejs/kit" />
+declare module '$env/static/private' {
+    export const AUTH0_CLIENT_SECRET: string;
+    export const AUTH0_SECRET: string;
+    export const AUTH0_SCOPE: string;
+}
+  
